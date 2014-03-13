17aa4e803a2d43f8f937d0d51a26b17b561eb5a2
Eriks <eriks.klotins@gmail.com>
2014-03-13 15:30:47 +0200

    Versija standalone darbināšanai ar standarta WP tēmām

---
+++ the-framework.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: WP Framework
+ * Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+ * Description: A brief description of the Plugin.
+ * Version: 1.0
+ * Author: Name Of The Plugin Author
+ * Author URI: http://URI_Of_The_Plugin_Author
+ * License: A "Slug" license name e.g. GPL2
+ */
+	
+
+//ALIAS for loader.php
+require 'loader.php';
